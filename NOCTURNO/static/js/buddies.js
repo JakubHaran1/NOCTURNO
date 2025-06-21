b636a7167289e89c4476a44d5af60823ede2ff31
JakubHaran <jakub.haran02@gmail.com>
2025-06-21 10:01:54 +0200

    refactoring buddies code

--- NOCTURNO/static/js/buddies.js
+++ NOCTURNO/static/js/buddies.js
@@ -1,24 +1,36 @@
 "use strict";
 import { menuFunction, show } from "./base.js";
 
-// SearchingNav
-const navSearch = document.querySelector(".search-nav");
-let searchOption = navSearch.querySelector(".active");
+class Buddies {
+  searchRow = document.querySelector(".search-row");
+  // SearchingNav
+  navSearch = document.querySelector(".search-nav");
+  searchOption = this.navSearch.querySelector(".active");
 
-// Search buddies
-const searchRow = document.querySelector(".search-row");
+  // Searching <- właściwe wyszukiwanie
+  searchingBtn = document.querySelector(".searching-btn");
+  searchingInput = document.querySelector(".search input");
 
-// Sending data link
-const getData = async function (link) {
-  let searchRowHtml = "";
-  let data;
-  try {
-    const fetchResponse = await fetch(link);
-    if (!fetchResponse.ok) throw new Error("Wrong link ✋✋");
-    data = await fetchResponse.json();
-    searchRow.textContent = "";
-    data.forEach((el) => {
-      searchRowHtml += `
+  constructor() {
+    // Unique
+    this.navSearch.addEventListener("click", this.changeSearch.bind(this));
+    this.searchingBtn.addEventListener(
+      "click",
+      this.searchingBuddie.bind(this)
+    );
+  }
+
+  // Funkcja pomocnicza do pobierania danych i tworzenia userbox
+  async getData(link) {
+    let searchRowHtml = "";
+    let data;
+    try {
+      const fetchResponse = await fetch(link);
+      if (!fetchResponse.ok) throw new Error("Wrong link ✋✋");
+      data = await fetchResponse.json();
+      this.searchRow.textContent = "";
+      data.forEach((el) => {
+        searchRowHtml += `
     <div class="buddy-box">
         <img src="{% static "${el.avatar}" %}" alt="${el.username}">
         <div class="buddy-info">
@@ -30,50 +42,40 @@ const getData = async function (link) {
         </div>
     </div>
     `;
-    });
-    searchRow.insertAdjacentHTML("afterbegin", searchRowHtml);
-  } catch (error) {
-    throw new Error(error);
+      });
+      this.searchRow.insertAdjacentHTML("afterbegin", searchRowHtml);
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-};
 
-// Zmiana typów wyszukiwań (dla find init value)
-const changeSearch = async function (e) {
-  const el = e.target.closest(".search-link");
+  // Zmiana typów wyszukiwań (dla find init value)
+  async changeSearch(e) {
+    const el = e.target.closest(".search-link");
+    console.log(el);
 
-  if (!el) return;
-  searchOption.classList.remove("active");
-  searchOption = el;
-  searchOption.classList.add("active");
-  console.log(searchOption);
-  document.cookie = `searchingType=${el.textContent}`;
-  const fetchLink = "buddies/initial-find";
-  getData(fetchLink);
-  // if (el.textContent == "Find") {
-  //   const fetchLink = "buddies/initial-find";
-  //   getData(fetchLink);
-  // }else{
+    if (!el) return;
+    this.searchOption.classList.remove("active");
+    this.searchOption = el;
+    this.searchOption.classList.add("active");
+    document.cookie = `searchingType=${el.textContent}`;
+    const fetchLink = "buddies/initial-find";
+    this.getData(fetchLink);
+  }
 
-  // }
-};
+  async searchingBuddie(e) {
+    e.preventDefault();
 
-// Searching <- właściwe wyszukiwanie
-const searchingBtn = document.querySelector(".searching-btn");
-const searchingInput = document.querySelector(".search input");
-
-const searchingBuddie = async function (e) {
-  e.preventDefault();
-
-  const value = searchingInput.value.trim().toLowerCase();
-  if (value == "") return;
-  const fetchLink = `/buddies/find-buddie/?nick=${value}`;
-  getData(fetchLink);
-};
-
-// Unique
-navSearch.addEventListener("click", changeSearch);
-searchingBtn.addEventListener("click", searchingBuddie);
+    const value = this.searchingInput.value.trim().toLowerCase();
+    if (value == "") return;
+    const fetchLink = `/buddies/find-buddie/?nick=${value}`;
+    getData(fetchLink);
+  }
+}
 
 // Basic function
 document.addEventListener("DOMContentLoaded", menuFunction);
 window.addEventListener("scroll", show);
+
+// Class
+const buddies = new Buddies();
